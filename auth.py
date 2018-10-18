dc80d8c2810931a3a0224efb91153461c1297131
Prajwal Krishna <prajwal.krishna@students.iiit.ac.in>
2018-10-18 10:58:59 +0530

    First phase generates token makes a new repo

---
+++ auth.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import getpass
+import os
+#import util
+#from autoissue import injectNumber
+#from urlparse import urljoin
+
+
+API_URL = 'https://api.github.com'
+SETTINGS = 'settings.williames' #settings file
+HEADERS = {'Content-type':'application/json'}
+TOKEN_KEY = 'auth_token'
+
+def getToken():
+	val = reviveToken(TOKEN_KEY)
+	if val is not None:
+		return val
+
+	#generate a token
+	username = input('Github username: ')
+	password = getpass.getpass('Github password: ')
+
+	url = API_URL+'/authorizations'
+	payload = {'note' : 'auto-hacktoberfest-tshirt', 'scopes' : ['repo']}
+	r = requests.post(url, auth = (username, password), data = json.dumps(payload),)
+
+	if r.status_code is requests.codes['created']:
+		token = json.loads(r.text or r.content)['token']
+		if not storeToken(TOKEN_KEY, token):
+			print("Could not write authorization token to settings file. Please add the following line to " + SETTINGS + ":\n" + "auth_token " + token)
+		return token
+	else:
+		print("Failed to generate a new authorization token")
+		print (r.text)
+		return None
+
+def reviveToken(key):
+	if os.path.exists(SETTINGS):
+		with open(SETTINGS) as f:
+			for line in f:
+				if key in line:
+					return line.split(" ", 1)[1].strip(" \n")
+	return None
+
+def storeToken(key, value):
+	with open(SETTINGS, "a+") as sett: # Will create the file if it does not exist
+		sett.write(key + " " + value + "\n")
+		return True
+
+	return False
+
+def starMaker(owner,repo):
+    url = API_URL + '/user/starred/' + owner + '/' + repo
+    url = url + "?access_token=" +getToken()
+    try:
+        r = requests.put(url)
+    except:
+        pass
+
+def createNewRepo(name):
+    url = API_URL + '/user/repos'
+    url = url + "?access_token=" +getToken()
+    data = {"name":name}
+    try:
+        r = requests.post(url,data = json.dumps(data),headers = HEADERS)
+    except:
+        print("Can't create repo")
+
+def getRepo():
+    url = API_URL + '/user/repos'
+    url = url + "?access_token=" + getToken()
+    try:
+        r = requests.get(url)
+        response = json.loads(r.content.decode("utf8"))
+        for i in response:
+            print(i["name"])
+    except:
+        print("Can't get repo lists")
+
+
+if __name__ == "__main__":
+    print(getToken())
+    #starMaker('pk1210','SuperMario')
+    getRepo()
+    createNewRepo('hi')
