77d51e277b4e0adaf2c9b0d2b3d98c8059b796a5
Prajwal Krishna <prajwal.krishna@students.iiit.ac.in>
2018-10-18 12:22:47 +0530

    Can make branch now

--- auth.py
+++ auth.py
@@ -11,44 +11,49 @@ API_URL = 'https://api.github.com'
 SETTINGS = 'settings.williames' #settings file
 HEADERS = {'Content-type':'application/json'}
 TOKEN_KEY = 'auth_token'
+USER_NAME = ''
+
+REPO_NAME = "hacktoberfest-free-tshirt"
+BRANCH_NAME = "nice-pants"
 
 def getToken():
-	val = reviveToken(TOKEN_KEY)
-	if val is not None:
-		return val
+    val = reviveToken(TOKEN_KEY)
+    if val is not None:
+        return val
 
-	#generate a token
-	username = input('Github username: ')
-	password = getpass.getpass('Github password: ')
+    #generate a token
+    username = input('Github username: ')
+    USER_NAME = username
+    password = getpass.getpass('Github password: ')
 
-	url = API_URL+'/authorizations'
-	payload = {'note' : 'auto-hacktoberfest-tshirt', 'scopes' : ['repo']}
-	r = requests.post(url, auth = (username, password), data = json.dumps(payload),)
+    url = API_URL+'/authorizations'
+    payload = {'note' : 'auto-hacktoberfest-tshirt', 'scopes' : ['repo']}
+    r = requests.post(url, auth = (username, password), data = json.dumps(payload),)
 
-	if r.status_code is requests.codes['created']:
-		token = json.loads(r.text or r.content)['token']
-		if not storeToken(TOKEN_KEY, token):
-			print("Could not write authorization token to settings file. Please add the following line to " + SETTINGS + ":\n" + "auth_token " + token)
-		return token
-	else:
-		print("Failed to generate a new authorization token")
-		print (r.text)
-		return None
+    if r.status_code is requests.codes['created']:
+        token = json.loads(r.text or r.content)['token']
+        if not storeToken(TOKEN_KEY, token):
+            print("Could not write authorization token to settings file. Please add the following line to " + SETTINGS + ":\n" + "auth_token " + token)
+        return token
+    else:
+        print("Failed to generate a new authorization token")
+        print (r.text)
+        return None
 
 def reviveToken(key):
-	if os.path.exists(SETTINGS):
-		with open(SETTINGS) as f:
-			for line in f:
-				if key in line:
-					return line.split(" ", 1)[1].strip(" \n")
-	return None
+    if os.path.exists(SETTINGS):
+        with open(SETTINGS) as f:
+            for line in f:
+                if key in line:
+                    return line.split(" ", 1)[1].strip(" \n")
+    return None
 
 def storeToken(key, value):
-	with open(SETTINGS, "a+") as sett: # Will create the file if it does not exist
-		sett.write(key + " " + value + "\n")
-		return True
+    with open(SETTINGS, "a+") as sett: # Will create the file if it does not exist
+        sett.write(key + " " + value + "\n")
+        return True
 
-	return False
+    return False
 
 def starMaker(owner,repo):
     url = API_URL + '/user/starred/' + owner + '/' + repo
@@ -78,9 +83,35 @@ def getRepo():
     except:
         print("Can't get repo lists")
 
+def getHashFromBranch(BranchName,author,repo):
+    url = API_URL + '/repos/%s/%s/branches/%s' % (author,repo,BranchName)
+    url = url + "?access_token=" +getToken()
+    try:
+        r = requests.get(url)
+        response = json.loads(r.content.decode('utf-8'))
+        return response["commit"]["sha"]
+    except:
+        print("Cannot access hask from previous branch")
+
+
+def createBranch(NewBranchName,BranchFrom,author,repo):
+    url = API_URL + '/repos/%s/%s/git/refs' % (author,repo)
+    HashToBranchFrom = getHashFromBranch(BranchFrom,author,repo)
+    data = {
+                "ref":'refs/heads/'+NewBranchName,
+                "sha":HashToBranchFrom
+            }
+    url = url + "?access_token=" +getToken()
+    #try:
+    r = requests.post(url,data = json.dumps(data),headers = HEADERS)
+    print(r)
+    print(r.content)
+    #except:
+    #    pass
 
 if __name__ == "__main__":
     print(getToken())
     #starMaker('pk1210','SuperMario')
-    getRepo()
-    createNewRepo('hi')
+    #getRepo()
+    createBranch("Branch23","master","libe-pachilly","Test-repo")
+    #getHashFromBranch("master","libe-pachilly","Test-repo")
